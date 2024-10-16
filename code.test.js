53078222da14636d8a80a75bb33ef7ff50277638
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-16 16:30:52 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,16 +1,35 @@
 const fs = require('fs');
 const jsc = require('jsverify');
+eval(fs.readFileSync('code.js') + '');
 
-eval(fs.readFileSync('code.js')+'');
+const graphA = {
+    0: [[1, 1], [2, 2]],
+    1: [[0, 1], [2, 1]],
+    2: [[0, 2], [1, 1]],
+};
 
-const testSearch =
-    jsc.forall("array nat", function(arr) {
-        if(arr.length > 0) {
-            arr.sort(function(a, b) { return a - b; });
-            return binarySearch(arr, arr[0]) === 0;
-        } else {
-            return binarySearch(arr, "foo") === -1;
-        }
-    });
+const graphB = {
+    0: [[1, 1], [2, 2]],
+    1: [[0, 1], [2, 1]],
+    2: [[0, 2], [1, 1]],
+};
 
-jsc.assert(testSearch);
+const graphC = {
+    0: [[1, 1]],
+    1: [[0, 1], [2, 1]],
+    2: [[1, 1]],
+};
+
+const isoTests = [
+    { graph1: graphA, graph2: graphB, expected: true, name: "Graphs A and B isomorphism test" },
+    { graph1: graphA, graph2: graphC, expected: false, name: "Graphs A and C isomorphism test" },
+];
+
+isoTests.forEach(test => {
+    const output = are_isomorphic(test.graph1, test.graph2);
+    if (output === test.expected) {
+        console.log(`${test.name} successful`);
+    } else {
+        console.error(`${test.name} failed: ${output} != ${test.expected}`);
+    }
+});
